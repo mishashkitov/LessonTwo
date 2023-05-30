cfa77ab2f0d66538c511762388b976ad5a155372
mishashkitov <shkitovm@gmail.com>
2023-05-30 19:21:44 +0500

    show/hide greeting method

--- LessonTwo/ViewController.swift
+++ LessonTwo/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingButton: UIButton!
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
     }
-
+    
+    @IBAction func greetingButtonWasTapped() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(greetingLabel.isHidden ? "Show greeting" : "Hide greeting", for: .normal)
+       
+    }
+    
 
 }
 
