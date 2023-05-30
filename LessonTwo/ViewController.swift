2fe070720ca7bbbe1ddd9c4fee679562f68f1c9b
mishashkitov <shkitovm@gmail.com>
2023-05-30 15:46:43 +0500

    Initial Commit

---
+++ LessonTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonTwo
+//
+//  Created by Misha Shkitov on 30.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
